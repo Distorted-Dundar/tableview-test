fe39fcf8e2d8bb0ebbc75baf8d7535b0ce9d081b
B. Flores <56740397+Distorted-Dundar@users.noreply.github.com>
2021-08-31 16:11:42 -0400

    Initial Commit

---
+++ 2tableview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2tableview
+//
+//  Created by Brian Flores on 8/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
